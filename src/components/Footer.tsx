df73c6de449853640bc817bb7d7001763f35995b
zxMiraie <94130957+zxMiraie@users.noreply.github.com>
2024-04-08 14:18:33 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer>© zx111 - not a real copyright btw</footer>
+    <footer>© zx111 - not a real copyright</footer>
     );
 }
 

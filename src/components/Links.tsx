28ded6eefe9d1e1ea35a00754a864ac24c6e2e32
zx <teodorsula1@gmail.com>
2025-02-11 09:50:49 +0000

    oops

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -7,7 +7,7 @@ const Links: React.FC = () => {
             <li><a href="https://www.last.fm/user/zx111"><FontAwesomeIcon icon={faSquareLastfm} size="2xl"/></a></li>
             <li><a href="https://github.com/zxMiraie"><FontAwesomeIcon icon={faSquareGithub} size="2xl"/></a></li>
             <li><a href="https://steamcommunity.com/id/zx111/"><FontAwesomeIcon icon={faSquareSteam} size="2xl"/></a></li>
-            <li><a href="https://www.flickr.com/photos/202230096@N02/"><FontAwesomeIcon icon={faFlickr}/></a></li>
+            <li><a href="https://www.flickr.com/photos/202230096@N02/"><FontAwesomeIcon icon={faFlickr} size="2xl"/></a></li>
         </ul>
 );
 };

a61839c408012499421464194aa953910ef8257a
zx <teodorsula1@gmail.com>
2025-02-11 09:47:20 +0000

    Flickr link

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faSquareLastfm, faSquareGithub, faSquareSteam} from '@fortawesome/free-brands-svg-icons'
+import {faSquareLastfm, faSquareGithub, faSquareSteam, faFlickr} from '@fortawesome/free-brands-svg-icons'
 const Links: React.FC = () => {
     return (
         <ul>
             <li><a href="https://www.last.fm/user/zx111"><FontAwesomeIcon icon={faSquareLastfm} size="2xl"/></a></li>
             <li><a href="https://github.com/zxMiraie"><FontAwesomeIcon icon={faSquareGithub} size="2xl"/></a></li>
             <li><a href="https://steamcommunity.com/id/zx111/"><FontAwesomeIcon icon={faSquareSteam} size="2xl"/></a></li>
+            <li><a href="https://www.flickr.com/photos/202230096@N02/"><FontAwesomeIcon icon={faFlickr}/></a></li>
         </ul>
 );
 };
